e2ba80f67eec8b2d110f22a4f7d1304e4b86e25c
darshan-m-corp <darshan.m@hashicorp.com>
2025-05-23 10:10:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,7 +77,15 @@ resource "aws_s3_object" "webapp" {
   content_type = "text/html"
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.bucket.id
 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
 
 
